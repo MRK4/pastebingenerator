c6316f642c4ea8522a1105a8b3ab8cdfeeba8032
clement.pdr <39833357+MRK4@users.noreply.github.com>
2021-01-11 05:18:00 +0100

    v1.0.1 (make the interface beautiful)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import string
+from colorama import Fore
 
 def generator():
     result = []
@@ -9,15 +10,24 @@ def generator():
             result.append(random.choice(string.ascii_letters))
         elif r == 2:
             result.append(random.randint(0, 9))
-
     print("https://pastebin.com/",*result, sep = '')
 
-print('#########################')
+print(Fore.YELLOW)
 print('')
-print('Welcome to the Pastebin Generator !')
+print('    ____             __       __    _       ______                           __            ')
+print('   / __ \____ ______/ /____  / /_  (_)___  / ____/__  ____  ___  _________ _/ /_____  _____')
+print('  / /_/ / __ `/ ___/ __/ _ \/ __ \/ / __ \/ / __/ _ \/ __ \/ _ \/ ___/ __ `/ __/ __ \/ ___/')
+print(' / ____/ /_/ (__  ) /_/  __/ /_/ / / / / / /_/ /  __/ / / /  __/ /  / /_/ / /_/ /_/ / /    ')
+print('/_/    \__,_/____/\__/\___/_.___/_/_/ /_/\____/\___/_/ /_/\___/_/   \__,_/\__/\____/_/     ')
 print('')
-print('#########################')
+print('                                                                                     by MRK')
 print('')
+print(Fore.RESET)
+print('Welcome to the PastebinGenerator !')
+print('')
+print(Fore.RED)
+print('please remember that a lot of the links are dead, use a checker or test them yourself.')
+print(Fore.RESET)
 
 linksWanted = int(input('How many links do you want ?       '))
 
@@ -26,5 +36,11 @@ def makeRandom():
     while (finishedLinks < linksWanted):
         randomShit = generator()
         finishedLinks = finishedLinks + 1
-
+        
+print('')
 makeRandom()
+
+print(Fore.LIGHTGREEN_EX)
+print('')
+print("Done!")
+print(Fore.RESET)
